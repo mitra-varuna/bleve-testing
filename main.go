545cf7cbf7b95a36363e6472c1160ef364446010
Sushant Srivastava <sushant.srivastav@gmail.com>
2016-06-21 16:37:56 +0530

    Added example

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/blevesearch/bleve"
+)
+
+type School struct {
+	ID          int
+	Name        string
+	Description string
+}
+
+func main() {
+
+	mapping := bleve.NewIndexMapping()
+	index, err := bleve.New("example.bleve", mapping)
+
+	if err == bleve.ErrorIndexPathExists {
+
+	}
+
+	if err != nil {
+		fmt.Println("Error in creating index")
+		fmt.Println(err)
+		return
+	}
+
+	newSchool := School{ID: 1, Name: "Test School", Description: "Test Description"}
+
+	err = index.Index("1", newSchool)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Search
+	query := bleve.NewMatchQuery("test")
+	search := bleve.NewSearchRequest(query)
+	results, err := index.Search(search)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(results)
+
+}
